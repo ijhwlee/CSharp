47d77feb337785a38500d742e003d79996112f9c
ijhwlee <hwlee@inje.ac.kr>
2022-11-15 22:08:30 +0900

    Check for command arguments

--- Chap01/HelloCS/HelloCS/Program.cs
+++ Chap01/HelloCS/HelloCS/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+if (args.Length > 0)
+{
+  //foreach (var arg in args)
+  for(var i = 0; i < args.Length; i++)
+  {
+    Console.WriteLine($"Argument[{i}]={args[i]}");
+  }
+}
+else
+{
+  Console.WriteLine("No arguments");
+}
