99adaea12847556cd3baec0cca0ec9b23833175e
ijhwlee <hwlee@inje.ac.kr>
2022-11-17 19:14:35 +0900

    adding Key Input

--- Chap02/Chap02/Formatting/Program.cs
+++ Chap02/Chap02/Formatting/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
 
 int numberOfApples = 12;
 decimal pricePerApple = 0.35M;
 
-Console.WriteLine(
+WriteLine($"There are {args.Length} arguments.");
+
+WriteLine(
   format: "{0} apples costs {1:C}",
   arg0: numberOfApples,
   arg1: pricePerApple * numberOfApples);
@@ -14,3 +17,25 @@ string formatted = string.Format(
   arg1: pricePerApple * numberOfApples);
 
 //WriteToFile(formatted);
+
+Write("Type your first name and press ENTER: ");
+string? firstName = ReadLine();
+
+Write("Type your age and press ENTER: ");
+string? age = ReadLine();
+
+WriteLine(
+  $"Hello {firstName}, you look for {age}.");
+
+while (true)
+{
+  Write("Press any key combination(Press Ctrl-Z to stop): ");
+  ConsoleKeyInfo key = ReadKey();
+  WriteLine();
+  WriteLine("Key: {0}, Char: {1}, Modifier: {2}",
+    arg0: key.Key,
+    arg1: key.KeyChar,
+    arg2: key.Modifiers);
+  if (key.Key == ConsoleKey.Z && key.Modifiers == ConsoleModifiers.Control)
+    break;
+}
