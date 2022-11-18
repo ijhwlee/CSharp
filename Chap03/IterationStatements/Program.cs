202952b89190d46cd7b543620fd03b585a85abed
ijhwlee <hwlee@inje.ac.kr>
2022-11-18 23:34:18 +0900

    add a new project for loops

---
+++ Chap03/IterationStatements/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+int x = 0;
+
+while(x < 10)
+{
+  WriteLine(x);
+  x++;
+}
+
+int tries = 0;
+string? password;
+do
+{
+  Write("Enter your password: ");
+  password = ReadLine();
+  tries++;
+} while(password != "Pa$$w0rd" && tries < 10);
+if (tries < 10)
+{
+  WriteLine("Correct!");
+}
+else
+{
+  WriteLine("You are wrong as 10 times!");
+}
