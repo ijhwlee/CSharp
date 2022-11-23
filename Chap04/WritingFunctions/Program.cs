5f7c2bb9aae9480e5dd470d82fd36f9babcbf2ce
ijhwlee <hwlee@inje.ac.kr>
2022-11-23 22:46:13 +0900

    add Debugging

--- Chap04/WritingFunctions/Program.cs
+++ Chap04/WritingFunctions/Program.cs
@@ -2,8 +2,10 @@
 using static System.Console;
 
 //TimesTable(255);
-decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
-WriteLine($"You must pay {taxToPay} in tax.");
+//decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+//WriteLine($"You must pay {taxToPay} in tax.");
+//RunFibImperative();
+RunFibFunctional();
 
 static void TimesTable(byte number)
 {
@@ -53,4 +55,68 @@ static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
       break;
   }
   return amount * rate;
+}
+
+static string CardinalToOrdinal(int number)
+{
+  switch(number)
+  {
+    case 11:
+    case 12:
+    case 13:
+      return $"{number}th";
+    default:
+      int lastDigit = number % 10;
+      string suffix = lastDigit switch
+      {
+        1 => "st",
+        2 => "nd",
+        3 => "rd",
+        _ => "th"
+      };
+      return $"{number}{suffix}";
+  }
+}
+static int FibImperative(int term)
+{
+  if(term == 1)
+  {
+    return 0;
+  }
+  else if (term == 2)
+  {
+    return 1;
+  }
+  else
+  {
+    return FibImperative(term - 1) + FibImperative(term - 2);
+  }
+}
+
+static void RunFibImperative()
+{
+  for(int i=1; i <= 30; i++)
+  {
+    WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+      arg0: CardinalToOrdinal(i),
+      arg1: FibImperative(term: i));
+  }
+}
+
+static int FibFunctional(int term) =>
+  term switch
+  {
+    1 => 0,
+    2 => 1,
+    _ => FibFunctional(term - 1) + FibFunctional(term - 2)
+  };
+
+static void RunFibFunctional()
+{
+  for(int i=1; i<=30; i++)
+  {
+    WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+      arg0: CardinalToOrdinal(i),
+      arg1: FibFunctional(term: i));
+  }
 }
