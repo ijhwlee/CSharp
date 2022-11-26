ffef12da780845fa66d484781c78f683342bb4be
ijhwlee <hwlee@inje.ac.kr>
2022-11-26 22:30:14 +0900

    add debugging features

---
+++ Chap04/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AIConvergence;
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+
+static void Alpha()
+{
+  WriteLine("In ALpha");
+  Beta();
+}
+
+static void Beta()
+{
+  WriteLine("In Beta");
+  try 
+  {
+    Calculator.Gamma();
+  }
+  catch (Exception ex)
+  {
+    WriteLine($"Cauught this: {ex.Message}");
+    throw;
+  }
+}
