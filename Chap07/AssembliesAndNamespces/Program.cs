67aa2fac6f836be544b0be753ca06229398b1c1d
ijhwlee <hwlee@inje.ac.kr>
2022-12-01 23:10:50 +0900

    add Chap07

---
+++ Chap07/AssembliesAndNamespces/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+using static System.Console;
+
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
+
+WriteLine($"int.MaxValue = {int.MaxValue:N0}");
+WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CSharp
 C# projects
+
+2022/12/1
+Solution Explorer 에서 Dependencies 에서 Analyzers에서 경고아이콘이 나타나는 경우에 
+해당 Project의 *.csproj 파일에서 <TargetFramework>net6.0</TargetFramework>을 수정하여 원하는 것으로 
+수정하면 문제가 해결된다. 
