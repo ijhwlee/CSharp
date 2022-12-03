d67bbdb7639c61d2eb36d90d1e2e7a370bf78438
ijhwlee <hwlee@inje.ac.kr>
2022-12-03 12:04:26 +0900

    build nuget package

---
+++ Chap07/AIConvergenceLibrary/Readme.md
@@ -0,0 +1,3 @@
+﻿This is the first Nuget package file for test.
+
+0.9.1 contains a minor bug fix for Hex check
--- Chap07/AssembliesAndNamespces/Program.cs
+++ Chap07/AssembliesAndNamespces/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AIConvergence.Shared;
 using System.Xml.Linq;
 using static System.Console;
 
@@ -10,3 +11,18 @@ WriteLine($"{s1} {s2}");
 
 WriteLine($"int.MaxValue = {int.MaxValue:N0}");
 WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
+
+Write("Enter a color value in hex: ");
+string? hex = ReadLine();
+WriteLine("Is {0} a valid color value? {1}",
+  arg0: hex, arg1: hex.IsValidHex());
+
+Write("Enter a XML element: ");
+string? xmlTag = ReadLine();
+WriteLine("Is {0} a valid XML element? {1}",
+  arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+
+Write("Enter a password: ");
+string? password = ReadLine();
+WriteLine("Is {0} a valid password? {1}",
+  arg0: password, arg1: password.IsValidPassword());
