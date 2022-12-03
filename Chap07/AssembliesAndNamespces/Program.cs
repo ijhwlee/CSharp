f615d4b8da5177aa5ef1c5c5ca5da728866e69c8
ijhwlee <hwlee@inje.ac.kr>
2022-12-03 16:45:25 +0900

    finishing Chapter 07

--- Chap07/AssembliesAndNamespces/Program.cs
+++ Chap07/AssembliesAndNamespces/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AIConvergence.Shared;
+using DialectSoftware.Collections;
+using DialectSoftware.Collections.Generics;
 using System.Xml.Linq;
 using static System.Console;
 
@@ -26,3 +28,30 @@ Write("Enter a password: ");
 string? password = ReadLine();
 WriteLine("Is {0} a valid password? {1}",
   arg0: password, arg1: password.IsValidPassword());
+
+Axis x = new Axis("x", 0, 10, 1);
+Axis y = new("y", 0, 4, 1);
+
+Matrix<long> matrix = new(new[] { x, y });
+for(int i=0; i < matrix.Axes[0].Points.Length; i++)
+{
+  matrix.Axes[0].Points[i].Label = "x" + i.ToString();
+}
+
+for (int i = 0; i < matrix.Axes[1].Points.Length; i++)
+{
+  matrix.Axes[1].Points[i].Label = "y" + i.ToString();
+}
+
+foreach (long[] c in matrix)
+{
+  matrix[c] = c[0] + c[1];
+}
+
+foreach (long[] c in matrix)
+{
+  WriteLine("{0},{1} ({2}, {3}) = {4}",
+    matrix.Axes[0].Points[c[0]].Label,
+    matrix.Axes[1].Points[c[1]].Label,
+    c[0], c[1], matrix[c]);
+}
---
+++ Chap07/UsingPreviewFeatures/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Versioning;
+using static System.Console;
+
+Doer.DoSomething();
+
+[RequiresPreviewFeatures]
+public interface IWithStaticAbstract
+{
+  static abstract void DoSomething();
+}
+
+[RequiresPreviewFeatures]
+public class Doer : IWithStaticAbstract
+{
+  public static void DoSomething()
+  {
+    WriteLine("I am an implementation of a static abstract method.");
+  }
+}
