9e7685317f29dcb2a377a53e4a16c54ac11d00b8
ijhwlee <hwlee@inje.ac.kr>
2022-12-07 12:46:35 +0900

    add List and Dictionaries examples

---
+++ Chap08/WorkingWithDicrionaries/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+decimal number = 3;
+WriteLine($"Decimal size {Marshal.SizeOf(number)}, {number}");
+
+var keywords = new Dictionary<string, string>();
+keywords.Add("int", $"{Marshal.SizeOf<int>()*8}-bit integer data type");
+keywords.Add("long", $"{Marshal.SizeOf<long>() * 8}-bit integer data type");
+keywords.Add("float", $"{Marshal.SizeOf<float>() * 8}-bit single precision floating point number");
+keywords.Add("double", $"{Marshal.SizeOf<double>() * 8}-bit double precision floating point number");
+keywords.Add("decimal", $"{Marshal.SizeOf<decimal>() * 8}-bit high precision floating point number");
+
+WriteLine("Keywords and their definitions");
+foreach(KeyValuePair<string, string> item in keywords)
+{
+  WriteLine($"  {item.Key}: {item.Value}");
+}
+WriteLine($"The definition of long is {keywords["long"]}");
---
+++ Chap08/WorkingWithLists/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using static System.Console;
+
+var cities = new List<string>();
+cities.Add("London");
+cities.Add("Paris");
+cities.Add("Roma");
+cities.Add("서울");
+
+WriteLine("Initial list");
+foreach(string city in cities)
+{
+  WriteLine($" {city}");
+}
+WriteLine();
+
+WriteLine($"The first city is {cities[0]}");
+WriteLine($"The last city is {cities[cities.Count-1]}");
+
+cities.Insert(0, "Gimhae");
+
+WriteLine("After inserting Gimhae at index 0");
+foreach (string city in cities)
+{
+  WriteLine($" {city}");
+}
+WriteLine();
+
+cities.RemoveAt(1);
+cities.Remove("Roma");
+
+WriteLine("After removing two cities");
+foreach (string city in cities)
+{
+  WriteLine($" {city}");
+}
