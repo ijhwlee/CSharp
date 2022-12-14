6b93e97231cb4bb9dab1b067ce6e639da373a381
ijhwlee <hwlee@inje.ac.kr>
2022-12-14 17:41:19 +0900

    add chaper 11

---
+++ Chap11/LinqWithObjects/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+using static System.Console;
+
+LinqWithArrayOfStrings();
+LinqWithArrayOfExceptions();
+
+static void LinqWithArrayOfStrings()
+{
+  WriteLine("===================== LinqWithArrayOfStrings =====================");
+  var names = new string[] {"Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin",
+    "Toby", "Creed", "철수"};
+  //var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+  //var query = names.Where(NameLongerThanFour);
+  var query = names.Where(name => name.Length > 4)
+    .OrderBy(name => name.Length)
+    .ThenBy(name => name);
+
+  foreach(string item in query)
+  {
+    WriteLine(item);
+  }
+}
+
+static void LinqWithArrayOfExceptions()
+{
+  WriteLine("================ LinqWithArrayOfExceptions ======================");
+  var errors = new Exception[]
+  {
+    new ArgumentException(),
+    new SystemException(),
+    new IndexOutOfRangeException(),
+    new InvalidOperationException(),
+    new NullReferenceException(),
+    new InvalidCastException(),
+    new OverflowException(),
+    new DivideByZeroException(),
+    new ApplicationException()
+  };
+  var numberErrors = errors.OfType<ArithmeticException>();
+  foreach(var error in numberErrors)
+  {
+    WriteLine(error);
+  }
+}
+static bool NameLongerThanFour(string name)
+{
+  return name.Length > 4;
+}
