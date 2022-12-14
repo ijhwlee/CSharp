8afc2410ba955ef63c40954e7828d9e24a47c94c
ijhwlee <hwlee@inje.ac.kr>
2022-12-14 23:52:22 +0900

    add Linq examples

---
+++ Chap11/LinqWithEFCore/Northwind.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIConvergence.Shared;
+
+public class Northwind : DbContext
+{
+  public DbSet<Category> Categories { get; set; }
+  public DbSet<Product> Products { get; set; }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    //string path = Path.Combine(
+    //  Environment.CurrentDirectory, "Northwind.db");
+    //optionsBuilder.UseSqlite($"Filename={path}");
+
+    string connection = "Data Source=.\\SQLEXPRESS;" +
+      "Initial Catalog=Northwind;" +
+      "Integrated Security=true;" +
+      "MultipleActiveResultSets=true;";
+    optionsBuilder.UseSqlServer(connection);
+  }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<Product>()
+      .Property(product => product.UnitPrice)
+      .HasConversion<double>();
+  }
+}
--- Chap11/LinqWithEFCore/Program.cs
+++ Chap11/LinqWithEFCore/Program.cs
@@ -1,3 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AIConvergence.Shared;
+using Microsoft.EntityFrameworkCore;
 using static System.Console;
 
+FilterAndSort();
+JoinCategoriesAndProducts();
+
+static void FilterAndSort()
+{
+  WriteLine("====================== FilterAndSort ==================");
+  using (Northwind db = new())
+  {
+    DbSet<Product> allProducts = db.Products;
+    IQueryable<Product> filteredProducts =
+      allProducts.Where(product => product.UnitPrice < 10M);
+    IOrderedQueryable<Product> sortedAndFilteredProducts =
+      filteredProducts.OrderByDescending(product => product.UnitPrice);
+
+    var projectedProducts = sortedAndFilteredProducts
+      .Select(product => new
+      {
+        product.ProductId,
+        product.ProductName,
+        product.UnitPrice
+      });
+
+    WriteLine("Products that cost less than $10:");
+    //foreach(Product p in sortedAndFilteredProducts)
+    foreach (var p in projectedProducts)
+    {
+      WriteLine("{0}: {1} costs {2:$#,##0.00}",
+      p.ProductId, p.ProductName, p.UnitPrice);
+    }
+    WriteLine();
+  }
+}
+
+static void JoinCategoriesAndProducts()
+{
+  WriteLine("============== JoinCategoriesAndProducts =================");
+  using (Northwind db = new())
+  {
+    var queryJoin = db.Categories.Join(
+      inner: db.Products,
+      outerKeySelector: category => category.CategoryId,
+      innerKeySelector: product => product.CategoryId,
+      resultSelector: (c, p) =>
+      new { c.CategoryName, p.ProductName, p.ProductId })
+      .OrderBy( cp => cp.CategoryName);
+
+    foreach (var item in queryJoin)
+    {
+      WriteLine("{0}: {1} is in {2}",
+        arg0: item.ProductId,
+        arg1: item.ProductName,
+        arg2: item.CategoryName);
+    }
+    WriteLine();
+  }
+}
