53d87c218be9719c40050a392937977e6dd8d446
ijhwlee <hwlee@inje.ac.kr>
2022-12-17 08:58:30 +0900

    add async stream exmaple

---
+++ Chap12/AsyncEnumerate/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+await AsyncStreams();
+
+static async Task<int> AsyncStreams()
+{
+  WriteLine("=============== AsyncStreams ===============");
+  await foreach(int number in GetNumbersAsync())
+  {
+    WriteLine($"Number: {number}");
+  }
+  return 0;
+}
+async static IAsyncEnumerable<int> GetNumbersAsync()
+{
+  Random r = new();
+  await Task.Delay(r.Next(1500, 3000));
+  yield return r.Next(0, 1001);
+  await Task.Delay(r.Next(1500, 3000));
+  yield return r.Next(0, 1001);
+  await Task.Delay(r.Next(1500, 3000));
+  yield return r.Next(0, 1001);
+}
