8e8acc1b40b52b2402828a7ff51608e78082e9f6
ijhwlee <hwlee@inje.ac.kr>
2022-12-16 14:43:12 +0900

    add synchronization

---
+++ Chap12/SynchronizingResourceAccess/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+internal class Program
+{
+  static Random r = new Random();
+  static string Message;
+  static object conch = new object();
+  private static void Main(string[] args)
+  {
+    CallSeparateThreads();
+  }
+
+  static void CallSeparateThreads()
+  {
+    WriteLine("===================== CallSeparateThreads ===============");
+    WriteLine("Please wait for the tasks to complete.");
+    Stopwatch watch = Stopwatch.StartNew();
+    Task a = Task.Factory.StartNew(MethodA);
+    Task b = Task.Factory.StartNew(MethodB);
+    Task.WaitAll(new Task[] { a, b });
+    WriteLine();
+    WriteLine($"Results: {Message}");
+    WriteLine($"{watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.");
+  }
+
+  static void MethodA()
+  {
+    lock (conch)
+    {
+      for (int i = 0; i < 5; i++)
+      {
+        Thread.Sleep(r.Next(2000));
+        Message += "A";
+        Write(".");
+      }
+    }
+  }
+  static void MethodB()
+  {
+    lock (conch)
+    {
+      for (int i = 0; i < 5; i++)
+      {
+        Thread.Sleep(r.Next(2000));
+        Message += "B";
+        Write(",");
+      }
+    }
+  }
+}
