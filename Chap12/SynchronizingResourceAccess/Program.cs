462c1f0e46c9a2b17ad1555eb4e0bfe9013cebcf
ijhwlee <hwlee@inje.ac.kr>
2022-12-16 16:12:23 +0900

    lock with try catch example

--- Chap12/SynchronizingResourceAccess/Program.cs
+++ Chap12/SynchronizingResourceAccess/Program.cs
@@ -4,15 +4,33 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using static System.Console;
+using System.Diagnostics.Metrics;
 
 internal class Program
 {
   static Random r = new Random();
   static string Message;
   static object conch = new object();
+  static int Count;
   private static void Main(string[] args)
   {
     CallSeparateThreads();
+    Message += "-";
+    CallSeparateThreadsTry();
+  }
+
+  static void CallSeparateThreadsTry()
+  {
+    WriteLine("===================== CallSeparateThreads ===============");
+    WriteLine("Please wait for the tasks to complete.");
+    Stopwatch watch = Stopwatch.StartNew();
+    Task a = Task.Factory.StartNew(MethodATry);
+    Task b = Task.Factory.StartNew(MethodBTry);
+    Task.WaitAll(new Task[] { a, b });
+    WriteLine();
+    WriteLine($"Results: {Message}");
+    WriteLine($"{watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.");
+    WriteLine($"{Count} string modifications.");
   }
 
   static void CallSeparateThreads()
@@ -26,6 +44,7 @@ internal class Program
     WriteLine();
     WriteLine($"Results: {Message}");
     WriteLine($"{watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.");
+    WriteLine($"{Count} string modifications.");
   }
 
   static void MethodA()
@@ -36,6 +55,7 @@ internal class Program
       {
         Thread.Sleep(r.Next(2000));
         Message += "A";
+        Interlocked.Increment(ref Count);
         Write(".");
       }
     }
@@ -48,8 +68,57 @@ internal class Program
       {
         Thread.Sleep(r.Next(2000));
         Message += "B";
+        Interlocked.Increment(ref Count);
         Write(",");
       }
+    }
+  }
+  static void MethodATry()
+  {
+    try
+    {
+      if (Monitor.TryEnter(conch, TimeSpan.FromSeconds(15)))
+      {
+        for (int i = 0; i < 5; i++)
+        {
+          Thread.Sleep(r.Next(2000));
+          Message += "A";
+          Interlocked.Increment(ref Count);
+          Write(".");
+        }
+      }
+      else
+      {
+        WriteLine("Method A failed to enter a monitor lock.");
+      }
+    }
+    finally
+    {
+      Monitor.Exit(conch);
+    }
+  }
+  static void MethodBTry()
+  {
+    try
+    {
+      if (Monitor.TryEnter(conch, TimeSpan.FromSeconds(15)))
+      {
+        for (int i = 0; i < 5; i++)
+        {
+          Thread.Sleep(r.Next(2000));
+          Message += "B";
+          Interlocked.Increment(ref Count);
+          Write(",");
+        }
+      }
+      else
+      {
+        WriteLine("Method B failed to enter a monitor lock.");
+      }
+    }
+    finally
+    {
+      Monitor.Exit(conch);
     }
   }
 }
