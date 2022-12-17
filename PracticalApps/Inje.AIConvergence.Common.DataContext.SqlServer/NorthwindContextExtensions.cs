0b243ba127060af5551dafe750275cd506612ab9
ijhwlee <hwlee@inje.ac.kr>
2022-12-17 17:42:51 +0900

    add SqlServer db libraries

---
+++ PracticalApps/Inje.AIConvergence.Common.DataContext.SqlServer/NorthwindContextExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIConvergence.Shared;
+
+public static class NorthwindContextExtensions
+{
+  public static IServiceCollection AddNorthwindContext(
+    this IServiceCollection services, string connectionString =
+    "Data Source=.\\SQLEXPRESS;" + "Initial Catalog=Northwind;" +
+    "Integrated Security=true;" + "MultipleActiveResultSets=true;")
+  {
+    services.AddDbContext<NorthwindContext>(options =>
+      options.UseSqlServer(connectionString));
+    return services;
+  }
+}
