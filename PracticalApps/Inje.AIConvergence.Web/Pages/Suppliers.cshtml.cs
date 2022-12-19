9ad12cea66894ef8a105ab95ee69e15e1b88cc64
ijhwlee@gmail.com <hwlee@inje.ac.kr>
2022-12-19 19:19:07 +0900

    add dependency injection to razor page

--- PracticalApps/Inje.AIConvergence.Web/Pages/Suppliers.cshtml.cs
+++ PracticalApps/Inje.AIConvergence.Web/Pages/Suppliers.cshtml.cs
@@ -8,6 +8,8 @@ public class SuppliersModel : PageModel
 {
   private NorthwindContext db;
   public IEnumerable<Supplier>? Suppliers { get; set; }
+  [BindProperty]
+  public Supplier? Supplier { get; set; }
   public SuppliersModel(NorthwindContext injectedContext)
   {
     db = injectedContext;
@@ -17,5 +19,18 @@ public class SuppliersModel : PageModel
     ViewData["Title"] = "Inje University AI Convergence B2B - Suppliers";
     Suppliers = db.Suppliers
       .OrderBy(c => c.Country).ThenBy(c => c.CompanyName);
+  }
+  public IActionResult OnPost()
+  {
+    if((Supplier is not null) && ModelState.IsValid)
+    {
+      db.Suppliers.Add(Supplier);
+      db.SaveChanges();
+      return RedirectToAction("/suppliers");
+    }
+    else
+    {
+      return Page();
+    }
   }
 }
