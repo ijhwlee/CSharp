eaa702ea5efc9394211f289f01265324990105f9
ijhwlee@gmail.com <hwlee@inje.ac.kr>
2022-12-19 21:27:24 +0900

    add partial view and razor class usage

---
+++ PracticalApps/Inje.AIConvergence.Razor.Employees/Areas/AIConvergenceFeatures/Pages/Employees.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Inje.AIConvergence.Shared;
+
+namespace AIConvergenceFeatures.Pages;
+
+public class EmployeesPageModel : PageModel
+{
+  private NorthwindContext db;
+  public Employee[] Employees { get; set; } = null;
+  public EmployeesPageModel(NorthwindContext db)
+  {
+    this.db = db;
+  }
+
+  public void OnGet()
+  {
+    ViewData["Title"] = "Inje University AI Convergence B2B - Employees";
+    Employees = db.Employees.OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToArray();
+  }
+}
