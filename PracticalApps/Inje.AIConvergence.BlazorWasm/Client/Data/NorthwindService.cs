1ed51f239aae03c35cbe252bb3aef347be217a5c
ijhwlee@gmail.com <hwlee@inje.ac.kr>
2022-12-25 13:23:30 +0900

    adding blazor webassembly

---
+++ PracticalApps/Inje.AIConvergence.BlazorWasm/Client/Data/NorthwindService.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http.Json;
+using Inje.AIConvergence.Shared;
+
+namespace Inje.AIConvergence.BlazorWasm.Client.Data;
+
+public class NorthwindService : INorthwindService
+{
+  private readonly HttpClient http;
+  public NorthwindService(HttpClient http)
+  {
+    this.http = http;
+  }
+
+  public async Task<Customer?> CreateCustomerAsync(Customer customer)
+  {
+    HttpResponseMessage response = await http.PostAsJsonAsync("api/customers", customer);
+    return await response.Content.ReadFromJsonAsync<Customer>();
+  }
+
+  public async Task DeleteCustomerAsync(string id)
+  {
+    HttpResponseMessage response = await http.DeleteAsync($"api/customers/{id}");
+  }
+
+  public Task<Customer?> GetCustomerAsync(string id)
+  {
+    return http.GetFromJsonAsync<Customer>($"api/customers/{id}");
+  }
+
+  public Task<List<Customer>?> GetCustomersAsync()
+  {
+    return http.GetFromJsonAsync<List<Customer>>("api/customers");
+  }
+
+  public Task<List<Customer>?> GetCustomersAsync(string country)
+  {
+    return http.GetFromJsonAsync<List<Customer>>($"api/customers/in/{country}");
+  }
+
+  public async Task<Customer?> UpdateCustomerAsync(Customer customer)
+  {
+    HttpResponseMessage response = await http.PutAsJsonAsync("api/customers", customer);
+    return await response.Content.ReadFromJsonAsync<Customer>();
+  }
+}
--- PracticalApps/Inje.AIConvergence.BlazorWasm/Client/Program.cs
+++ PracticalApps/Inje.AIConvergence.BlazorWasm/Client/Program.cs
@@ -1,4 +1,6 @@
 using Inje.AIConvergence.BlazorWasm.Client;
+using Inje.AIConvergence.BlazorWasm.Client.Data;
+using Inje.AIConvergence.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddTransient<INorthwindService, NorthwindService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
