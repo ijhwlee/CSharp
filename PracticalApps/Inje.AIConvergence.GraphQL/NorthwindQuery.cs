e3f5b9e15fc282ba8a293bdbab74d0f1aa546c68
ijhwlee <hwlee@inje.ac.kr>
2022-12-26 20:20:53 +0900

    make do function for GraphQL version 7.2.1 for EF queries

--- PracticalApps/Inje.AIConvergence.GraphQL/NorthwindQuery.cs
+++ PracticalApps/Inje.AIConvergence.GraphQL/NorthwindQuery.cs
@@ -10,33 +10,42 @@ public class NorthwindQuery : ObjectGraphType
 {
   public NorthwindQuery(NorthwindContext db)
   {
-    WriteLine($"[DEBUG-hwlee]NorthwindQuery: db = {db}");
+    Field<IntGraphType, int>("test").Resolve(_ => 123);
+    Field<NonNullGraphType<StringGraphType>>("name")
+      .Description("Argument name")
+      .Resolve(context => db.Employees.FirstOrDefault<Employee>()?.EmployeeId.ToString());
+    Field<ListGraphType<CategoryType>>("namesCategory")
+      .Description("Argument names of Category")
+      .Resolve(context => db.Categories);
+    //WriteLine($"[DEBUG-hwlee]NorthwindQuery: db = {db}");
     Field<ListGraphType<CategoryType>>("categories")
       .Description("A query type that returns a list of all categories.")
       .Resolve(context => db.Categories.Include(c => c.Products));
 
-    Func<IResolveFieldContext, int, Category?> func = (context, id) =>
-    {
-      Category? category = db.Categories.Find(id);
-      db.Entry(typeof(Category)).Collection("Products").Load();
-      return category;
-    };
-
     Field<CategoryType>("category")
       .Description("A query type that returns a category using its Id.")
       .Argument<IntGraphType>("categoryId", "id of the category")
-      .ResolveDelegate(func);
+      .Resolve(context =>
+      {
+        int id = context.GetArgument<int>("categoryId");
+        //WriteLine($"[DEBUG-hwlee]NorthwindQuery: category, id = {id}");
+        Category? category = db.Categories.Find(context.GetArgument<int>("categoryId"));
+        if (category != null)
+        {
+          category.Products = db.Products.Where(p => p.CategoryId == id).ToArray<Product>();
+        }
+        return category;
+      });
 
-    //Func<IResolveFieldContext, int, ListGraphType<ProductType>> func1 = (context, id) =>
-    //{
-    //  Category? category = db.Categories.Find(id);
-    //  db.Entry(typeof(Category)).Collection("Products").Load();
-    //  return category.Products;
-    //};
-
-    //Field<ListGraphType<ProductType>>("products")
-    //  .Description("A query type that returns a category using its Id.")
-    //  .Argument<IntGraphType>("categoryId", "id of the category")
-    //  .ResolveDelegate(func1);
+    Field<ListGraphType<ProductType>>("products")
+      .Description("A query type that returns a products using if given category Id.")
+      .Argument<IntGraphType>("categoryId", "id of the category")
+      .Resolve(context =>
+      {
+        int id = context.GetArgument<int>("categoryId");
+        //WriteLine($"[DEBUG-hwlee]NorthwindQuery: products, id = {id}");
+        Product[] products = db.Products.Where(p => p.CategoryId == id).ToArray<Product>();
+        return products;
+      });
   }
 }
--- PracticalApps/Inje.AIConvergence.GraphQL/NorthwindSchema.cs
+++ PracticalApps/Inje.AIConvergence.GraphQL/NorthwindSchema.cs
@@ -9,10 +9,10 @@ public class NorthwindSchema : Schema
 {
   public NorthwindSchema(IServiceProvider provider) : base(provider)
   {
-    WriteLine("[DEBUG-hwlee]NorthwindSchema ===============");
+    //WriteLine("[DEBUG-hwlee]NorthwindSchema ===============");
     //Query = provider.GetRequiredService<GreetQuery>();
     //Query = new GreetQuery();
-    WriteLine($"[DEBUG-hwlee]NorthwindSchema, provider = {provider}");
+    //WriteLine($"[DEBUG-hwlee]NorthwindSchema, provider = {provider}");
     //Query = new NorthwindQuery(provider.GetService<NorthwindContext>());
     Query = new NorthwindQuery(provider.GetRequiredService<NorthwindContext>());
   }
