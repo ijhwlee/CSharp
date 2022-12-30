165a92c965a40a76dbb4cb370678180392470eef
ijhwlee@gmail.com <hwlee@inje.ac.kr>
2022-12-30 14:58:10 +0900

    add simple identity exmaple

--- AspNetCore/Inje.AIConvergence.WebApp/Program.cs
+++ AspNetCore/Inje.AIConvergence.WebApp/Program.cs
@@ -2,6 +2,8 @@ using Inje.AIConvergence.WebApp.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
+using static System.Console;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +15,38 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+  // Password settings.
+  options.Password.RequireDigit = true;
+  options.Password.RequireLowercase = true;
+  options.Password.RequireNonAlphanumeric = true;
+  options.Password.RequireUppercase = true;
+  options.Password.RequiredLength = 6;
+  options.Password.RequiredUniqueChars = 1;
+
+  // Lockout settings.
+  options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+  options.Lockout.MaxFailedAccessAttempts = 5;
+  options.Lockout.AllowedForNewUsers = true;
+
+  // User settings.
+  options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+  options.User.RequireUniqueEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+  // Cookie settings
+  options.Cookie.HttpOnly = true;
+  options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+  options.LoginPath = "/Identity/Account/Login";
+  options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+  options.SlidingExpiration = true;
+});
 
 var app = builder.Build();
 
@@ -36,6 +70,9 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
