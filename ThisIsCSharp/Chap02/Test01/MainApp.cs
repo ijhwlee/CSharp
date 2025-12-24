97ae053a28d9788e64bcd8f84d1b772256e6118b
Lee Hyung Won <ijhwlee@gmail.com>
2025-12-25 08:50:35 +0900

    adding Chap02

---
+++ ThisIsCSharp/Chap02/Test01/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+namespace Test01
+{
+  internal class MainApp
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length > 0)
+      {
+        WriteLine("매개변수 목록:");
+        foreach (var arg in args)
+        {
+          WriteLine("매개변수 : {0}!", arg);
+        }
+      }
+      else
+      {
+        WriteLine("사용법 : Test01.exe [이름 ...]");
+      }
+    }
+  }
+}
