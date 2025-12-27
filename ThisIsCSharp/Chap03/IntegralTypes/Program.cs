c32b7aac19486592dbfa1b6602ad182ed9f6a44b
Lee Hyung Won <ijhwlee@gmail.com>
2025-12-28 06:36:46 +0900

    add interaltypes example

---
+++ ThisIsCSharp/Chap03/IntegralTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace IntegralTypes
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      sbyte a = -10;
+      byte b = 40;
+      Console.WriteLine($"a = {a}, b = {b}");
+      short c = -30000;
+      ushort d = 60000;
+      Console.WriteLine($"c = {c}, d = {d}");
+      long g = -5000_0000_0000;
+      ulong h = 200_0000_0000_0000_0000;
+      Console.WriteLine($"g = {g}, h = {h}");
+    }
+  }
+}
