1cfc549f2b94aaa5ff413f1383d440e53d483de6
Lee Hyung Won <ijhwlee@gmail.com>
2026-01-01 01:28:10 +0900

    add test04

---
+++ ThisIsCSharp/Chap04/Test04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Test04
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int a = 0xF0 | 0x0F;
+      Console.WriteLine($"a : {a}");
+    }
+  }
+}
