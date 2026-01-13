888efa86fbfeee1f6b3ebd322b7809c9aadc5a1a
Lee Hyung Won <ijhwlee@gmail.com>
2026-01-13 17:10:02 +0900

    chap05 exmaple add

---
+++ ThisIsCSharp/Chap05/IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
