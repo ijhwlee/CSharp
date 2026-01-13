d6395002b72671c2cfad4ce46a4309b6140b41bc
Lee Hyung Won <ijhwlee@gmail.com>
2026-01-13 17:18:41 +0900

    example 수정

--- ThisIsCSharp/Chap05/IfElse/Program.cs
+++ ThisIsCSharp/Chap05/IfElse/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 namespace IfElse
 {
-    internal class Program
+  internal class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+      Console.WriteLine("숫자를 입력하세요...");
+      string? input = Console.ReadLine();
+      int number = Int32.Parse(input);
+      if (number < 0)
+      {
+        Console.WriteLine("입력한 숫자는 음수입니다.");
+      }
     }
+  }
 }
